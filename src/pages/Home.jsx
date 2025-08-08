556291075c8dfa18a4f433acfe429cdb499ddf53
Fatimih Linz <fatimihlinz@Fatimihs-Laptop.local>
2025-08-08 14:57:44 -0700

    Add route pages: Home, About, Work, Contact

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,7 @@
-function Home() {
+export default function Home() {
   return (
-    <main>
+    <div>
       <h1>Home Page</h1>
-      <p>This is the placeholder for the homepage.</p>
-    </main>
+    </div>
   );
 }
-
-export default Home;
