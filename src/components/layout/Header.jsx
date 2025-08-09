51b0f18b339e1fc5daf6f6db3f33d2f935e5f91c
Fatimih Linz <fatimihlinz@Fatimihs-Laptop.local>
2025-08-09 12:08:35 -0700

    03.06: Add CSS Modules and scope Header styles

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,8 +1,9 @@
 import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
 
 export default function Header() {
   return (
-    <header>
+    <header className={styles.root}>
       <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
