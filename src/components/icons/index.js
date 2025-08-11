cb38a0d2ddd4d6ee499f7e1a2e62859d307133fe
Fatimih Linz <fatimihlinz@Fatimihs-Laptop.local>
2025-08-11 13:50:55 -0700

    Chore: add barrel export for icons component

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,9 +1,1 @@
-/**
- * Centralized icon exports.
- * Only import icons from this file elsewhere in the app.
- * If we switch libraries later, we change it here once.
- */
-export { FaArrowRight } from 'react-icons/fa';
-// Add more as you use them:
-// export { FaExternalLinkAlt } from 'react-icons/fa';
-// export { FiMail } from 'react-icons/fi';
+export { default as Icon } from './Icon.jsx';
